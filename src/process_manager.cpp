ef8cc8a267bddbf809a55c8eb158dc3095156201
ayushpr1741 <ayushpanwar01032005@gmail.com>
2025-04-04 10:08:18 +0530

    Bg FG and Kill job is added

--- src/process_manager.cpp
+++ src/process_manager.cpp
@@ -34,6 +34,75 @@ void listJobs() {
     }
 }
 
+void fg(int jobId) {
+    for (auto &job : jobList) {
+        if (job.id == jobId) {
+            cout << "Bringing job [" << job.id << "] to foreground...\n";
+            WaitForSingleObject(job.hProcess, INFINITE); 
+            CloseHandle(job.hProcess);
+            job.isRunning = false;
+            return;
+        }
+    }
+    cout << "Error: Job ID not found.\n";
+}
+
+void bg(int jobId) {
+    for (auto &job : jobList) {
+        if (job.id == jobId) {
+            cout << "Resuming job [" << job.id << "] in background...\n";
+            ResumeThread(job.hProcess);
+            job.isRunning = true;
+            return;
+        }
+    }
+    cout << "Error: Job ID not found.\n";
+}
+
+void killJob(int jobId) {
+    for (auto &job : jobList) {
+        if (job.id == jobId) {
+            cout << "Killing job [" << job.id << "]...\n";
+            TerminateProcess(job.hProcess, 0);
+            CloseHandle(job.hProcess);
+            job.isRunning = false;
+            return;
+        }
+    }
+    cout << "Error: Job ID not found.\n";
+}
+
 int main() {
+    STARTUPINFO si = { sizeof(si) };
+    PROCESS_INFORMATION pi;
+
     
+    if (CreateProcess(NULL, (LPSTR)"notepad.exe", NULL, NULL, FALSE,
+                      CREATE_SUSPENDED, NULL, NULL, &si, &pi)) {
+        addJob(pi.hProcess, pi.dwProcessId, "notepad.exe");
+
+        
+        ResumeThread(pi.hThread);
+        CloseHandle(pi.hThread); 
+    } else {
+        cout << "Failed to start notepad.exe\n";
+        return 1;
+    }
+
+    Sleep(2000); 
+    listJobs(); 
+    Sleep(1000);
+    fg(1); 
+    Sleep(1000);
+
+    if (CreateProcess(NULL, (LPSTR)"notepad.exe", NULL, NULL, FALSE,
+                      0, NULL, NULL, &si, &pi)) {
+        addJob(pi.hProcess, pi.dwProcessId, "notepad.exe");
+    }
+
+    Sleep(2000);
+    listJobs(); 
+    killJob(2); 
+
+    return 0;
 }
